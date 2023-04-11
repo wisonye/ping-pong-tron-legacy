70cc08e5359f9334a0046d43d9ce020b4d6b65de
Wison Ye <wisonye@gmail.com>
2023-04-11 13:53:08 +1200

    [ master ] - Fixed 'run_zig_cc.sh'

--- run_zig_cc.sh
+++ run_zig_cc.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 rm -rf zig_build && mkdir zig_build
 
-CFLAGS=$(pkg-config --cflags raylib)
-LIB_FLAGS=$(pkg-config --libs-only-L raylib)
-LINK_FLAGS=$(pkg-config --libs-only-l raylib)
-# echo "CFLAGS: ${CFLAGS}, LIB_FLAGS: ${LIB_FLAGS}, LINK_FLAGS: ${LINK_FLAGS}"
+CFLAGS="-pedantic-errors -Wall -Werror -Wextra -std=c11 -O3 -DNDEBUG"
 
-echo -e "Compile cmd: \nzig cc -I${CFLAGS} \
-    -L${LIB_FLAGS} \
-    ${LINK_FLAGS} \
+RAYLIB_CFLAGS=$(pkg-config --cflags raylib)
+RAYLIB_LIB_FLAGS=$(pkg-config --libs-only-L raylib)
+RAYLIB_LINK_FLAGS=$(pkg-config --libs-only-l raylib)
+
+echo -e "Compile cmd: \nzig cc ${CFLAGS} \
+    ${RAYLIB_CFLAGS} \
+    ${RAYLIB_LIB_FLAGS} \
+    ${RAYLIB_LINK_FLAGS} \
     -o zig_build/my-game \
     src/*.c \
     && ./zig_build/my-game\n\n"
 
-zig cc -I${CFLAGS} \
-    -L${LIB_FLAGS} \
-    ${LINK_FLAGS} \
+zig cc ${CFLAGS} \
+    ${RAYLIB_CFLAGS} \
+    ${RAYLIB_LIB_FLAGS} \
+    ${RAYLIB_LINK_FLAGS} \
     -o zig_build/my-game \
     src/*.c \
     && ./zig_build/my-game
